fedc1355bbaa71dd6016af32efc5d0f6aa94acc2
Dima Miro <d.miro.work@gmail.com>
2019-01-04 22:43:38 +0100

    Initial Commit

---
+++ Draft/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Draft
+//
+//  Created by Dima Miro on 04/01/2019.
+//  Copyright © 2019 Dima Miro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
